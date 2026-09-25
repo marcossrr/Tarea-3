d8c1a78695a27a7b9f351cbd8683145330a8ddb2
marcosrr <mariro@alumnatflorida.es>
2026-09-25 12:40:47 +0200

    Ejercicio 1 acabado

--- bebidas/ejercicio1.js
+++ bebidas/ejercicio1.js
@@ -2,23 +2,32 @@ let image = document.createElement('img');
 let secondimage = document.createElement('img');
 let thirdimage = document.createElement('img');
 let td = document.getElementsByTagName('td')[0];
-let title = document.getElementsByTagName('h1')[0];
+let title = document.getElementsByTagName('h1')[1];
 let secondTitle = document.createElement('h1');
 let thirdTitle = document.createElement('h1');
 
-image.src = "src/cafe/1.jpg";
-secondimage.src = "src/alcohol/1.jpg";
-thirdimage.src = "src/infusiones/1.jpg";
-
-
-
-title.textContent = "Cafe";
-title.style.backgroundImage = "image"
-secondTitle.textContent = "Alcohol";
-thirdTitle.textContent = "Infusiones";
-
- 
 td.appendChild(secondTitle);
 td.appendChild(thirdTitle);
+
+title.style.backgroundImage = "url('./src/cafe/1.jpg')";
+title.style.width = "450px";
+title.style.height = "300px";
+
+secondTitle.textContent = "Alcohol";
+secondTitle.style.backgroundImage = "url('./src/alcohol/1.jpg')";
+secondTitle.style.width = "450px";
+secondTitle.style.height = "300px";
+
+thirdTitle.textContent = "Infusiones";
+thirdTitle.style.backgroundImage = "url('./src/infusiones/1.jpg')";
+thirdTitle.style.width = "450px";
+thirdTitle.style.height = "300px";
+
+td.style.display = "flex";
+td.style.justifyContent = "center";
+td.style.width = "450px";
+td.style.height = "300px";
+ 
+
 
 
